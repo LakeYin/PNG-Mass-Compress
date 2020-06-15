2dab102e610b0182a0efc4069587781ec323d3c5
Lake Yin <la11010e@gmail.com>
2020-06-14 22:13:39 -0400

    Added analytics

--- png_compress.py
+++ png_compress.py
@@ -2,27 +2,30 @@ import os
 import cv2
 import argparse
 
-def compress(directory, level):
+def compress(directory, level, subs=0, files=0):
     if not os.path.isdir(directory):
         print("Error: " + directory + " is not a valid directory.")
-        return 1
+        return -1,-1
 
-    for file in os.listdir(directory): # check for subdirectories and recurse
+    for file in os.listdir(directory): # check through everything in the directory
         newpath = os.path.join(directory, file)
 
-        if(os.path.isdir(newpath)):
+        if(os.path.isdir(newpath)): # recurse through subdirectories
             print("Recursing to subdirectory " + file)
-            compress(newpath, level)
+            subs += 1
+            record = compress(newpath, level) # keep track of the number of subdirectories and pngs
+            subs += record[0]
+            files += record[1]
 
-    for file in os.listdir(directory): # now check for PNGs and run compression
-        path = os.path.join(directory, file)
-
-        if(file.lower().endswith(".png") and os.path.isfile(path)):
+        elif(file.lower().endswith(".png")): # compress pngs
             try:
-                cv2.imwrite(path, cv2.imread(path, cv2.IMREAD_UNCHANGED), [cv2.IMWRITE_PNG_COMPRESSION, level])
+                cv2.imwrite(newpath, cv2.imread(newpath, cv2.IMREAD_UNCHANGED), [cv2.IMWRITE_PNG_COMPRESSION, level])
                 print("Compressed " + file)
+                files += 1
             except:
                 print("Error: could not compress " + file)
+
+    return (subs,files)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -30,7 +33,12 @@ def main():
     parser.add_argument("-l", "--level", type=int, default=9, help="the level of compression desired from 1 to 9 (default)")
     args = parser.parse_args()
 
-    compress(args.directory, args.level)
+    total = compress(args.directory, args.level)
+
+    if(total != (-1,-1)):
+        print("\nFinished compression.")
+        print("Navigated through " + str(total[0]) + " subdirectories")
+        print("Compressed " + str(total[1]) + " PNGs")
 
 if __name__ == "__main__":
     main()
